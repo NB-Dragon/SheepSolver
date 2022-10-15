6295f4e2db0257369028d39d90ae95bf3ce55897
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2022-10-15 14:45:17 +0800

    修改第三方的抓包处理逻辑

--- web/sheep.py
+++ web/sheep.py
@@ -1,4 +1,5 @@
 from mitmproxy import ctx
+from datetime import date
 import execjs
 import json
 import os
@@ -10,7 +11,7 @@ class Sheep():
         self.seed = [0, 0, 0, 0]
         self.js_code = open("shuffle.js", encoding="utf-8").read()
         self.map_data_path = "./map_data.txt"
-        self.solve_origin_path = "path/to/your/SheepSolver/online_data.json"
+        self.map_data_topic_path = "./map_data_topic.txt"
 
     def response(self, flow):
         """ 接口响应方法 """
@@ -18,29 +19,68 @@ class Sheep():
             # 获取随机种子的接口，随机种子存储到self.seed中
             response = json.loads(flow.response.content)
             self.seed = response["data"]["map_seed"]
-            self.make_map_data()
-        elif "maps" in flow.request.path:
+            self.make_map_data(False)
+        elif "topic/game_start" in flow.request.path:
+            # 今日话题获取随机种子的接口
+            response = json.loads(flow.response.content)
+            self.seed = response["data"]["map_seed"]
+            self.make_map_data(True)
+        elif "/maps/" in flow.request.path:
             # 获取地图数据的接口，不解析第一关
             if "046ef1bab26e5b9bfe2473ded237b572" in flow.request.path:
+                # 游戏第一关
                 return
-            # 保存地图数据
+            if "a92ee0f5f116b13b7b594e67a53defad" in flow.request.path:
+                # 今日话题第一关
+                return
+            # 解析原始地图数据
             response = json.loads(flow.response.content)
-            with open(self.map_data_path, "w") as f:
+            # 判断是否是话题挑战
+            is_topic = False
+            if response["levelKey"] == 100000 + date.today().day:
+                is_topic = True
+            # 保存原始地图数据
+            map_data_path = self.get_map_data_path(is_topic)
+            with open(map_data_path, "w") as f:
                 f.write(json.dumps(response, indent=4))
                 f.close()
-            self.make_map_data()
+            self.make_map_data(is_topic)
 
-    def make_map_data(self):
+    def get_map_data_path(self, is_topic):
+        """ 获取文件路径 """
+        if is_topic:
+            return self.map_data_topic_path
+        else:
+            return self.map_data_path
+    
+    def write_outside_solve_data(self, origin_data):
+        save_path = "/home/am/文档/PycharmProjects/SheepSlover"
+        slove_map_data = {}
+        for level, data_list in origin_data["levelData"].items():
+            slove_map_data[level] = []
+            for data_item in data_list:
+                slove_item = {"id": data_item["id"],
+                "type": data_item["type"],
+                "rolNum": data_item["rolNum"],
+                "rowNum": data_item["rowNum"]}
+                slove_map_data[level].append(slove_item)
+        writer = open(save_path, "w")
+        writer.write(json.dumps(slove_map_data))
+        writer.close()
+        
+
+    def make_map_data(self, is_topic):
         """ 制作地图数据 """
 
         print("==========================================")
 
-        # 判断地图文件是否存在
-        if not os.path.exists(self.map_data_path):
+        # 判断原始地图文件是否存在
+        map_data_path = self.get_map_data_path(is_topic)
+        if not os.path.isfile(map_data_path):
             return
 
         # 读取原始地图数据
-        map_data = json.loads(open(self.map_data_path).read())
+        map_data = json.loads(open(map_data_path).read())
 
         # 根据"blockTypeData"字段按顺序生成所有类型的方块，存放到数组
         block_type_data = map_data["blockTypeData"]
@@ -52,13 +92,14 @@ class Sheep():
                 block_types.extend([i] * count)
 
         # 调用js方法将数组打乱，打乱后的结果和游戏的一样
-        block_types = execjs.compile(self.js_code).call("shuffle", block_types, self.seed)
+        block_types = execjs.compile(self.js_code).call(
+            "shuffle", block_types, self.seed)
         print(block_types)
 
         # 将游戏的层数排序
         level_data = map_data["levelData"]
         layers = list(level_data.keys())
-        layers.sort(key=lambda x:int(x))
+        layers.sort(key=lambda x: int(x))
 
         # 为了方便three.js按顺序读取层数，所以将层数保存起来
         map_data["layers"] = layers
@@ -74,26 +115,12 @@ class Sheep():
 
         # 保存地图数据
         data_string = f"const map_data = {json.dumps(map_data, indent=4)};"
-        save_path = "./three.js/examples/map_data.js"
+        save_path = "./html/map_data.js"
         with open(save_path, "w") as f:
             f.write(data_string)
             f.close()
 
         print("==========================================")
-
-        slove_map_data = {}
-        for level, data_list in map_data["levelData"].items():
-            slove_map_data[level] = []
-            for data_item in data_list:
-                slove_item = {"type": data_item["type"],
-                "min_x": data_item["rolNum"],
-                "min_y": data_item["rowNum"],
-                "max_x": data_item["rolNum"] + 8,
-                "max_y": data_item["rowNum"] + 8}
-                slove_map_data[level].append(slove_item)
-        writer = open(self.solve_origin_path, "w")
-        writer.write(json.dumps(slove_map_data))
-        writer.close()
 
 
 addons = [Sheep()]
