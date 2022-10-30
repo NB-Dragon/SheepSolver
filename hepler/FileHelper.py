d7b4a75d07043d01c0a12f6e75a810336cc378ec
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2022-10-31 02:27:37 +0800

    添加文件写入的功能

--- hepler/FileHelper.py
+++ hepler/FileHelper.py
@@ -1,11 +1,15 @@
 import json
-import os.path
+import os
 
 
 class FileHelper(object):
     def read_json_data(self, file_path):
         file_content = self.read_file_content(file_path) or "null"
         return json.loads(file_content)
+
+    def write_json_data(self, file_path, json_data):
+        json_content = json.dumps(json_data)
+        self.write_file_content(file_path, json_content)
 
     @staticmethod
     def read_file_content(file_path):
@@ -19,3 +23,12 @@ class FileHelper(object):
                 return None
         except Exception as e:
             return None
+
+    @staticmethod
+    def write_file_content(file_path, content):
+        try:
+            writer = open(file_path, "w")
+            writer.write(content)
+            writer.close()
+        except Exception as e:
+            print("文件写入失败: {}, 出错详情为: {}".format(file_path, str(e)))
