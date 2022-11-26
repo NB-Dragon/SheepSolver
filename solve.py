62181a65fba0bfd0b72484050375b78ad9a4ae66
iversonpuppets <41983159+iversonpuppets@users.noreply.github.com>
2022-11-26 09:43:34 +0800

    Quality of life improvement
    添加command line argument接口
    执行python solve.py 1 默认模式
    执行python solve.py 2 反转模式
    执行python solve.py 3 随机模式
    执行python solve.py 4 高层优先模式

--- solve.py
+++ solve.py
@@ -7,6 +7,21 @@ def read_online_data():
     online_file_path = "online_data.json"
     return FileHelper().read_json_data(online_file_path)
 
+#normal: 默认模式，该模式下取牌顺序由程序进度决定
+#reverse: 反转模式，该模式下取牌顺序由卡牌的逆序号决定，序号大的优先
+#random: 随机模式，该模式下取牌顺序完全随机
+#top-first: 高层优先模式，该模式下取牌顺序由卡牌的层数决定，高层优先
+mode=sys.argv[1]
+if mode == "1":
+	mode="normal"
+elif mode == "2":
+	mode="reverse"
+elif mode == "3":
+	mode="random"
+elif mode == "4":
+	mode="top-first"
+else:
+	mode="normal"
 
 if __name__ == '__main__':
     sheep_solver = SheepSolver("normal", 0.8, True)
