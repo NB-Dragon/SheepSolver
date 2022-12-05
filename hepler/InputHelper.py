5fc3e83e86d05bec69aecd73c5754eecbf78798a
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2022-12-05 21:53:04 +0800

    添加参数启动逻辑

---
+++ hepler/InputHelper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Create Time: 2022/11/30 00:00
+# Create User: NB-Dragon
+import argparse
+import time
+
+
+class InputHelper(object):
+    def __init__(self):
+        self._continue_checking = True
+
+    def get_runtime_arguments(self, ):
+        try:
+            parser = argparse.ArgumentParser()
+            parser.add_argument("-m", "--mode", dest='mode', required=True, help="The mode to solve game.")
+            args = parser.parse_args()
+            return args if self._check_input_args(args) else None
+        except SystemExit:
+            return None
+
+    def _check_input_args(self, args):
+        if not self._check_solve_mode_correct(args.mode):
+            self._send_error_message("The solve mode must be [normal|random|reverse|top-first]")
+        return self._continue_checking
+
+    @staticmethod
+    def _check_solve_mode_correct(solve_mode):
+        return solve_mode in ["normal", "random", "reverse", "top-first"]
+
+    def _send_error_message(self, message):
+        self._continue_checking = False
+        self._make_message_and_send(message)
+
+    @staticmethod
+    def _make_message_and_send(content):
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        output_content = "[{}][{}]: {}".format(current_time, "ParserHelper", content)
+        print(output_content)
